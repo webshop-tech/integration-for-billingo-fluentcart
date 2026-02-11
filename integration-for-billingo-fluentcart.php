1d55c764b3ad6e36f9f6c3c86d9806819a5c4bc7
Gabor Angyal <gabor.angyal@codesharp.dev>
2026-02-11 11:47:04 +0100

    fix refund hook

--- integration-for-billingo-fluentcart.php
+++ integration-for-billingo-fluentcart.php
@@ -142,6 +142,7 @@ function get_pdf_path($invoice_number) {
 }, 10, 1);
 
 \add_action('fluent_cart/order_refunded', function($data) {
+    $order = $data['order'];
     cancel_invoice($order);
 }, 10, 1);
 
